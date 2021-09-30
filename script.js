38213fd6e309f42b6ea265f0e36623a1daeb4de9
miguebalderrama <42938621+miguebalderrama@users.noreply.github.com>
2021-09-29 23:25:21 -0300

    agregue un contador delikes

--- script.js
+++ script.js
@@ -6,18 +6,24 @@ const parraf = document.createElement("p");
 const btn = document.createElement("button");
 const icon = document.createElement("i");
 const like = document.createElement("span");
-var gustado=0;
+const cont = document.createElement("span");
+let gustado = 0 ;
+
 
 
 image.src="https://picsum.photos/id/237/500/500";
 image.alt="Perro bebe";
-title.textContent= "Soy un perro bebe, dame likes para ser mas popular!";
+title.textContent= "Hola soy un perro bebe!";
 
 
 newDog.classList.add("dog");
-btn.classList.add("moreInfo");
+btn.classList.add("button");
+btn.textContent = "Clic para leer mas sobre mi";
+cont.textContent = 0;
 icon.classList.add("far");
 icon.classList.add("fa-heart");
+icon.style.cursor= "pointer";
+cont.style.margin="10px";
 like.classList.add("likes");
 
 document.body.appendChild(newDog);
@@ -26,10 +32,15 @@ newDog.appendChild(title);
 newDog.appendChild(parraf);
 newDog.appendChild(like);
 like.appendChild(icon);
+like.appendChild(cont);
 newDog.appendChild(btn);
+
+const showMore = () =>
+  (document.querySelector(".moreInfo").style.display = "block");
 
 const pushlike = ()=>{ 
  gustado=gustado+1;
+ cont.textContent=gustado;
  console.log(gustado);
 }
 
