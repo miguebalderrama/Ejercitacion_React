fd1d882bc28111efada5518c8624ec9927b4ef01
miguebalderrama <42938621+miguebalderrama@users.noreply.github.com>
2021-09-29 22:20:42 -0300

    trabajando en la tarjeta2

--- script.js
+++ script.js
@@ -4,17 +4,35 @@ const image = document.createElement("img");
 const title = document.createElement("h2");
 const parraf = document.createElement("p");
 const btn = document.createElement("button");
+const icon = document.createElement("i");
+const like = document.createElement("span");
+var gustado=0;
+
 
 image.src="https://picsum.photos/id/237/500/500";
 image.alt="Perro bebe";
-title.textContent= "Lorem lore lrelrlelrlelrlerelrlelr";
+title.textContent= "Soy un perro bebe, dame likes para ser mas popular!";
 
 
 newDog.classList.add("dog");
 btn.classList.add("moreInfo");
+icon.classList.add("far");
+icon.classList.add("fa-heart");
+like.classList.add("likes");
+
 document.body.appendChild(newDog);
 newDog.appendChild(image);
 newDog.appendChild(title);
 newDog.appendChild(parraf);
+newDog.appendChild(like);
+like.appendChild(icon);
+newDog.appendChild(btn);
+
+const pushlike = ()=>{ 
+ gustado=gustado+1;
+ console.log(gustado);
+}
+
+document.querySelector(".likes").addEventListener("click",pushlike);
 
 
