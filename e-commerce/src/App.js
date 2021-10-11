f2674e5af43a355e485f1184d819a420846b4bdd
miguebalderrama <42938621+miguebalderrama@users.noreply.github.com>
2021-10-11 00:29:23 -0300

    limpiando un poco

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,32 +1,13 @@
 import './App.css';
-import Dog from './components/Dog';
+import React, { useState } from "react";
 import NavBar from './components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import FunctionalComponent  from './components/functional-component/FunctionalComponent';
-import React, { useState } from "react";
 import ButtonMe from './components/buttonMe/ButtonMe';
 import ItemListContainer from './components/item-list-container/ItemListContainer';
 
-
-
-
-
 function App() {
   const [name, setName] = useState("");
-  const dog = {
-    name: "Bobby",
-    age: 12,
-    breed: "Chihuahua"
-  }
-  
-  const dog2={ ...dog, name:"Pipo", color:"black" };
-  const dogs=[
-    {name:"Boby", age:4},
-    {name:"Pipo", age:10},
-    {name:"Bromita", age:2}
-  ];
-  console.log(dog);
-  console.log(dog2);
   const hello = "Hola tio!"
   const handleClickButton = (nameButton) => setName(nameButton);
   return(
@@ -40,14 +21,7 @@ function App() {
     <ButtonMe className="parent-button2" action={()=> handleClickButton("Juan")} name={"Boton 2"}/>
     <ButtonMe className="parent-button3" action={()=> handleClickButton("Lucas")} name={"Boton 3"}/>
    
-    <hr/>
-   
-    {dogs.map((dog)=>(
-      <>
-    <Dog key={dog.name} {...dog}/>
-    <hr/>
-    </>
-    ))}
+    <hr/>   
 
   </div>
   );
